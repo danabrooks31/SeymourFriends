ff1fc3f5b058e61cc31c2c2848bf44a32b0aa4b9
Jennifer Liu <jennifer.liu2@duke.edu>
2026-02-27 14:33:32 -0500

    added some features to Board.jsx

---
+++ src/pages/Board.jsx
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <div>
+      <h1>Seymour Board</h1>
+      <MyButton />
+    </div>
+  );
+}
