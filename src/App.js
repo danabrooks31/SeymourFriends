688cb75f2da05aeb0079b0ca471f432ee70e6a2d
Dana Brooks <dkb31@duke.edu>
2026-03-16 22:41:52 -0400

    Added PDF and messed with about and board page, added this to the functionality of app.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from './pages/About';
+import  Board  from './pages/Board';
+import  Volunteer  from './pages/Volunteer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
+              </header>
+            </div>
+          }
+        />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/board" element={<Board/>}/>
+        <Route path="/volunteer" element={<Volunteer/>}/>
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
